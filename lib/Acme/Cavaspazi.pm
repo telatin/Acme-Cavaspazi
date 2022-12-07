87ed6983a579df56a76bb34330630d93dc99ee70
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2022-12-07 17:28:24 +0000

    ciao nicola!

---
+++ lib/Acme/Cavaspazi.pm
@@ -0,0 +1,10 @@
+package Acme::Cavaspazi;
+# ABSTRACT: a simple script to remove spaces from filenames or file contents, but also a tribute to old school robust bioinformatics tools
+
+use 5.012;
+use warnings;
+
+
+$Acme::Cavaspazi::VERSION = "0.1.0";
+
+1;
