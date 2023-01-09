b36b95d22a61f9060368dbe21212e7be27f0bc60
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2023-01-09 14:41:01 +0000

    Update

--- lib/Acme/Cavaspazi.pm
+++ lib/Acme/Cavaspazi.pm
@@ -9,7 +9,7 @@ our @ISA = qw(Exporter);
 # Export subroutine cavaspazi
 our @EXPORT = qw(cavaspazi);
 
-$Acme::Cavaspazi::VERSION = "1.0.0";
+$Acme::Cavaspazi::VERSION = "1.0.1";
 
 
 =head1 SYNOPSIS
